25b60ce1cbe64a2a874e4d1fe9a8d0e22b8acc32
khm31 <khm31@Kwon>
2025-12-26 23:14:46 +0900

    2025-12-26

--- web_servlet/src/main/java/web_servlet/servlet1.java
+++ web_servlet/src/main/java/web_servlet/servlet1.java
@@ -8,7 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+//연결확인
 /**
  * Servlet implementation class servlet1
  */
